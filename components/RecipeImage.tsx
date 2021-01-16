19702d799c8d0a3f82bc37548253dd73010459be
Matt Jones <matt.r.jones1995@gmail.com>
2021-01-16 20:01:37 +0000

    Wait for images to be downloaded before rendering

--- components/RecipeImage.tsx
+++ components/RecipeImage.tsx
@@ -1,5 +1,6 @@
 import { Recipe } from "../types/recipe";
 import Cook from "../pages/resources/cook1.svg";
+import { useEffect, useState } from "react";
 
 interface Props
   extends React.DetailedHTMLProps<
@@ -10,9 +11,11 @@ interface Props
 }
 
 export const RecipeImage = ({ recipe, ...rest }: Props) => {
-  const className = "h-48 w-full object-cover md:w-48";
   const src = recipe.images[0];
-  if (!src) {
+  const [image, status] = useImage(src);
+
+  const className = "h-48 w-full object-cover md:w-48";
+  if (!src || status == "failed") {
     return (
       <div {...rest} className={`bg-gray-50 ${className}`}>
         <Cook viewBox="0 0 700 750" width="100%" height="100%" />
@@ -20,5 +23,47 @@ export const RecipeImage = ({ recipe, ...rest }: Props) => {
     );
   }
 
-  return <img src={recipe.images[0]} {...rest} className={className} />;
+  if (status == "loading") {
+    return (
+      <div className="animate-pulse">
+        <div className={className}></div>
+      </div>
+    );
+  }
+
+  return <img src={image.src} className={className} {...rest} />;
+};
+
+var defaultState = { image: undefined, status: "loading" };
+
+const useImage = (url) => {
+  const [{ image, status }, setState] = useState(defaultState);
+
+  useEffect(() => {
+    if (!url) {
+      return;
+    }
+
+    const imgElement = new Image();
+
+    const onLoad = () => {
+      setState({ image: imgElement, status: "loaded" });
+    };
+
+    const onError = () => {
+      setState({ image: undefined, status: "failed" });
+    };
+
+    imgElement.addEventListener("load", onLoad);
+    imgElement.addEventListener("error", onError);
+    imgElement.src = url;
+
+    return () => {
+      imgElement.removeEventListener("load", onLoad);
+      imgElement.removeEventListener("error", onError);
+      setState(defaultState);
+    };
+  }, [url]);
+
+  return [image, status];
 };
