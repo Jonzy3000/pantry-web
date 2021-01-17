367c95bd190bee64b191b72abc29dff29da0f933
Matt Jones <matt@boclips.com>
2021-01-17 20:20:40 +0000

    Add an error view if search goes bad

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,18 +1,30 @@
 import { useRouter } from "next/router";
 import { useSearch } from "../api-queries/useSearch";
 import { RecipeSkeletonView, RecipeView } from "../components/RecipeView";
+import Error from "./resources/error.svg";
+
+const ErrorView = () => (
+  <div className="flex flex-col items-center justify-center">
+    <h1 className="text-lg md:text-2xl mt-20 md:mt-8 font-medium">
+      Ooops, couldn't fetch the recipe you were looking for
+    </h1>
+    <div className="w-full mt-8">
+      <Error viewBox="0 0 1200 700" width="100%" height="100%" />
+    </div>
+  </div>
+);
 
 const Search = () => {
   const { query } = useRouter();
   // nasty cast
-  const { status, data, error } = useSearch(query.q as string);
+  const { status, data } = useSearch(query.q as string);
 
   return (
     <div className="flex flex-col place-items-center">
       {status === "loading" ? (
         <RecipeSkeletonView />
       ) : status === "error" ? (
-        `Error: ${error}`
+        <ErrorView />
       ) : (
         <div>{data && <RecipeView recipe={data} />}</div>
       )}
