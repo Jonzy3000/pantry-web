555a38a092fda41b4b52b1b33db0cba5781dfc8a
Matt Jones <matt@boclips.com>
2021-01-17 19:04:27 +0000

    Fix build errors

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,10 +1,7 @@
 import { signIn } from "next-auth/client";
 import { useRouter } from "next/router";
 import { useSearch } from "../api-queries/useSearch";
-import { useUser } from "../api-queries/useUser";
-import { useUserMutation } from "../api-queries/useUserMutation";
 import { RecipeSkeletonView, RecipeView } from "../components/RecipeView";
-import { SearchBar } from "../components/SearchBar";
 
 const Search = () => {
   const { query } = useRouter();
