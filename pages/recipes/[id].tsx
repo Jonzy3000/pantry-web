bd2a0eaba713c0bdcb8904a32924fd1014238036
Matt Jones <matt.r.jones1995@gmail.com>
2021-04-02 12:53:29 +0100

    More tweaks to fix timeout isseues on netlify

--- pages/recipes/[id].tsx
+++ pages/recipes/[id].tsx
@@ -1,13 +1,7 @@
-import { useRouter } from "next/router";
 import { RecipeSkeletonView, RecipeView } from "../../components/RecipeView";
 import { findRecipeById } from "../../server/db/recipesRepository";
 
 const Recipe = ({ recipe }) => {
-  const router = useRouter();
-  if (router.isFallback) {
-    return <RecipeSkeletonView />;
-  }
-
   if (!recipe) {
     return <div>Not found...</div>;
   }
@@ -19,14 +13,7 @@ const Recipe = ({ recipe }) => {
   );
 };
 
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: true,
-  };
-}
-
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
   console.log(`static props of recipe: ${params.id}`);
   const recipe = await findRecipeById(params.id);
   console.log("found recipe in static props");
