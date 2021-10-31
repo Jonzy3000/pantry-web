e96b2a3ed84e32152b635a79ae2e6db3be689919
Matt Jones <matt@boclips.com>
2021-10-31 17:58:54 +0000

    Use latest next and next-auth

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 module.exports = {
-  target: "experimental-serverless-trace",
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      issuer: {
-        test: /\.tsx?$/,
-      },
       use: ["@svgr/webpack"],
     });
 
