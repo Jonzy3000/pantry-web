512fc09bf33fcbdf139ba113e86cfc3eecf2bb40
Matt Jones <matt@boclips.com>
2022-07-10 11:00:53 +0100

    Profile finding recipes of a user

--- server/api/recipes.ts
+++ server/api/recipes.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { performance } from "perf_hooks";
 import { findRecipesByIds } from "../db/recipesRepository";
 import { getUserFromSession } from "./me";
 
@@ -14,7 +15,12 @@ export default async function handler(
   }
 
   if (req.method === "GET") {
+    const startTime = performance.now();
     const recipes = await findRecipesByIds(user.recipes);
+    const endTime = performance.now();
+
+    console.log(`Finding recipes took ${endTime - startTime}ms`)
+
     res.status(200).json({ recipes });
   }
 }
